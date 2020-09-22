5d536adf283c9ae4e63c810283ce2009252bc4b8
JoshsBruhMoment <joshua.aldahondo@patelhs.org>
2020-09-22 16:22:38 +0000

    I added the integers to the code.

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+class Main
+{
+  public static void main(String[] args) 
+  {
+    int myAge = 15;
+    int myBirthday = 211;
+    int todaysDate = 922;
+  
   }
 }
