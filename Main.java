8c171af070972d4ae75dd65992c5b11141349852
JoshsBruhMoment <joshua.aldahondo@patelhs.org>
2020-09-22 16:35:50 +0000

    Ran a test to see if the if statement worked (myAge + 1) by changing the date of my birthday to todays date.

--- Main.java
+++ Main.java
@@ -3,11 +3,11 @@ class Main
   public static void main(String[] args) 
   {
     int myAge = 15;
-    int myBirthday = 211;
+    int myBirthday = 922;
     int todaysDate = 922;
   if(myBirthday == todaysDate)
   {
-System.out.println("I am " + (myAge + 1) + "years old.");
+System.out.println("I am " + (myAge + 1) + " years old.");
   }
   else 
   {
